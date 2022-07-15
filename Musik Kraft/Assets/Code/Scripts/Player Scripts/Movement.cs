40ebd887db4f712cec60dba5cdaf777128618755
b2200356018 <gozlu.oguz@gmail.com>
2022-07-15 19:02:06 +0300

    Jump / fall animation + smoothing

--- Musik Kraft/Assets/Code/Scripts/Player Scripts/Movement.cs
+++ Musik Kraft/Assets/Code/Scripts/Player Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -11,12 +12,16 @@ public class Movement : MonoBehaviour
     public float speed = 10f;
     public float jumpForce = 12f;
     public Animator animator;
+    public float acceleration = 3.5f;
+    public float decceleration = 13f;
+    public float velPower = 1f;
     
     
     private Vector2 dir;
     
     private bool jumpPressed;
     private bool isRunning;
+    private bool isGrounded;
     public bool isFacingRight = true;
     
     // Start is called before the first frame update
@@ -29,11 +34,18 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        float x = Input.GetAxisRaw("Horizontal");
         isRunning = Input.GetButton("Horizontal");
-        
+
         float y = Input.GetAxis("Vertical");
         dir = new Vector2(x, y);
+
+        isGrounded = GetComponent<DetectDirections>().onGround;
+        if (isGrounded != animator.GetBool("isGrounded"))
+        {
+            animator.SetBool("isGrounded", isGrounded);
+        }
+        
         
         if (Input.GetButtonDown("Jump"))
         {
@@ -46,22 +58,42 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Run(dir);
         
-        Walk(dir);
-        if (jumpPressed && GetComponent<DetectDirections>().onGround)
+        if (jumpPressed && isGrounded)
         {
             Jump();
             jumpPressed = false;
         }
-
+        
+        FallDetection();
+        
     }
 
 
-    private void Walk(Vector2 dir)
+    private void Run(Vector2 dir)
     {
-        
-        rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
         animator.SetBool("isRunning", isRunning);
+        //rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+
+        #region Run
+
+        float targetSpeed = dir.x * speed;
+        float speedDif = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+
+        rb.AddForce(movement * Vector2.right);
+        
+        #endregion
+
+
+
+        
+        
+
+        #region Flip
+
         if (dir.x > 0 && !isFacingRight)
         {
             Flip();
@@ -71,11 +103,16 @@ public class Movement : MonoBehaviour
             Flip();
         }
 
+        #endregion
+        
+        
+
 
     }
 
     public void Jump()
-    {
+    {   
+        animator.SetTrigger("jump");
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
@@ -86,6 +123,18 @@ public class Movement : MonoBehaviour
         transform.localScale = currentScale;
 
         isFacingRight = !isFacingRight;
+    }
+
+    void FallDetection()
+    {
+        if (rb.velocity.y < 0 )
+        {
+            animator.SetBool("isFalling", true);
+        }
+        else
+        {
+            animator.SetBool("isFalling", false);
+        }
     }
 
     
