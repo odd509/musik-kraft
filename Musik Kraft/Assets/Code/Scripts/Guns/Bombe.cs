b43cc2e6974ce212d690245fc48d4f819964b615
b21946088 <b21946088@cs.hacettepe.edu.tr>
2022-07-18 15:26:40 +0300

    3. silah gibimsi

--- Musik Kraft/Assets/Code/Scripts/Guns/Bombe.cs
+++ Musik Kraft/Assets/Code/Scripts/Guns/Bombe.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class GuitarWeapon : MonoBehaviour
+public class Bombe : MonoBehaviour
 {
 
     public Transform firePointFront;
@@ -26,7 +26,7 @@ public class GuitarWeapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             Shoot();
         }
@@ -37,15 +37,7 @@ public class GuitarWeapon : MonoBehaviour
         Vector2 projectileVector;
         var proj = Instantiate(bullet, firePointFront.position, Quaternion.identity); 
 
-        if (Input.GetKey(KeyCode.UpArrow) && (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
-        {
-            projectileVector = GetComponent<Movement>().isFacingRight ? (Vector2.right + Vector2.up).normalized : (Vector2.left + Vector2.up);
-
-            float rotationAmount = GetComponent<Movement>().isFacingRight ? 45 : -45;
-            
-            proj.transform.Rotate(0, 0, rotationAmount);
-        }
-        else if ((Input.GetKey(KeyCode.UpArrow)))
+        if ((Input.GetKey(KeyCode.UpArrow)))
         {
             projectileVector = Vector2.up;
             proj.transform.Rotate(0, 0,  90);
@@ -53,7 +45,11 @@ public class GuitarWeapon : MonoBehaviour
         }
         else
         {
-            projectileVector = GetComponent<Movement>().isFacingRight ? Vector2.right : Vector2.left;
+            projectileVector = GetComponent<Movement>().isFacingRight ? (Vector2.right + Vector2.up).normalized : (Vector2.left + Vector2.up).normalized;
+
+            float rotationAmount = GetComponent<Movement>().isFacingRight ? 45 : -45;
+            
+            proj.transform.Rotate(0, 0, rotationAmount);
         }
 
         
